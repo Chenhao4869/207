6ae5cbcf030e8a741534b6ea31729d0056a0dcbc
陈浩 <43244600+Chenhao4869@users.noreply.github.com>
2018-10-27 11:42:43 +0800

    Add files via upload

---
+++ ex4-4.5-1.py
@@ -0,0 +1,23 @@
+import random as r
+
+p = r.randint(0,100)
+
+count = 0;
+
+while True:
+    try:
+        n = int(input('请输入一个0-100之间的整数：'))
+    
+    except :
+        print("输入错误，请输入一个整数")
+        break
+    count += 1
+        
+    if n > p:
+         print('遗憾，太大了')
+    elif n == p:
+        print('预测{}次，你猜中了！'.format(count))
+        break
+    else:
+            print('遗憾，太小了')
+   
